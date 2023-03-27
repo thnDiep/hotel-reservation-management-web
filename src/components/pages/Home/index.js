ba5d9e13b185f933abcfe96a553b3231bb31743a
Tran-thaoquyen23 <92144715+Tran-thaoquyen23@users.noreply.github.com>
2023-03-27 18:37:30 +0700

    Tạo giao diện trang Profile

--- src/components/pages/Home/index.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <h1>Home page</h1>
-}
-
-export default Home
--- src/components/pages/ListByPlace/index.js
+++
@@ -1,5 +0,0 @@
-function ListByPlace() {
-    return <h1>List by Place page</h1>
-}
-
-export default ListByPlace
