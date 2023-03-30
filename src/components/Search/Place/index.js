d0d7354908dbddcde6321a02de5d4e12b8b0544d
thnDiep <thndiepkhtn@gmail.com>
2023-03-30 16:56:01 +0700

    Complete Seacrch Component

---
+++ src/components/Search/Place/index.js
@@ -0,0 +1,59 @@
+import styles from './Place.module.scss'
+import notablePlaces from './notable.json'
+
+function Place({ placeHistory, onClearHistory }) {
+    return (
+        <div className={styles.place}>
+            <div className={styles.historyPlace}>
+                <div className="d-flex">
+                    <div className={styles.title}>Tìm kiếm gần đây</div>
+                    {placeHistory.length !== 0 && <button onClick={onClearHistory}>Xóa lịch sử tìm kiếm</button>}
+                </div>
+
+                {placeHistory.length === 0 ? (
+                    <div className={styles.noneList}>Chưa có tìm kiếm gần đây</div>
+                ) : (
+                    <ul className={styles.list}>
+                        {placeHistory.map((place, index) => (
+                            <li key={index} className={styles.item}>
+                                <div className={styles.image}>
+                                    <svg width="20" height="20" fill="none">
+                                        <path
+                                            d="M10 6.667V10l1.667 1.667"
+                                            stroke="#718096"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        ></path>
+                                        <path
+                                            d="M2.542 9.167a7.5 7.5 0 11.416 3.333m-.416 4.167V12.5h4.166"
+                                            stroke="#718096"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        ></path>
+                                    </svg>
+                                </div>
+                                <div className={styles.info}>
+                                    <div className={styles.name}>{place}</div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+
+            <div className={styles.notablePlace}>
+                <div className={styles.title}>Địa điểm nổi bật</div>
+                <ul className={styles.list}>
+                    {notablePlaces.map((place, index) => (
+                        <li key={index}>
+                            <div className={styles.image} style={{ backgroundImage: `url(${place.image})` }}></div>
+                            <div className={styles.name}>{place.name}</div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Place
---
+++ src/components/Search/reducer/index.js
@@ -0,0 +1,108 @@
+import moment from 'moment'
+import { addDays } from 'date-fns'
+
+import { CHANGE_SHOW, INPUT_PLACE, CLEAR_PLACE_HISTORY, INPUT_DATE, INPUT_NUMBER, SUBMIT_SEARCH } from './constans'
+
+const initState = {
+    show: null,
+    place: '',
+    placeHistory: JSON.parse(localStorage.getItem('placeHistory')) || [],
+    date: {
+        startDate: new Date(),
+        endDate: addDays(new Date(), 2),
+        key: 'selection',
+        duration: 2,
+    },
+    number: {
+        room: { value: 1, disableDescrease: true },
+        adult: { value: 1, disableDescrease: true },
+        child: { value: 0, disableDescrease: true },
+    },
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case CHANGE_SHOW:
+            return {
+                ...state,
+                show: action.payload,
+            }
+        case INPUT_PLACE:
+            return {
+                ...state,
+                place: action.payload,
+            }
+        case CLEAR_PLACE_HISTORY:
+            return {
+                ...state,
+                placeHistory: [],
+            }
+        case INPUT_DATE:
+            return {
+                ...state,
+                date: {
+                    ...action.payload,
+                    duration: moment(action.payload.endDate).diff(action.payload.startDate, 'days'),
+                },
+            }
+        case INPUT_NUMBER:
+            const room = action.payload.room
+            const adult = action.payload.adult
+            const child = action.payload.child
+
+            if (room.value <= 1) {
+                room.value = 1
+                room.disableDescrease = true
+            } else {
+                room.disableDescrease = false
+            }
+
+            if (adult.value <= 1 || adult.value <= room.value) {
+                if (adult.value <= 1) {
+                    adult.value = 1
+                }
+                if (adult.value <= room.value) {
+                    adult.value = room.value
+                }
+                adult.disableDescrease = true
+            } else {
+                adult.disableDescrease = false
+            }
+
+            if (child.value <= 0) {
+                child.value = 0
+                child.disableDescrease = true
+            } else {
+                child.disableDescrease = false
+            }
+
+            return {
+                ...state,
+                number: { room, adult, child },
+            }
+        case SUBMIT_SEARCH:
+            const submit = {
+                place: state.place,
+                number: {
+                    room: state.number.room.value,
+                    adult: state.number.adult.value,
+                    child: state.number.child.value,
+                },
+                startDate: state.date.startDate,
+                endDate: state.date.endDate,
+            }
+            console.log(submit)
+
+            return {
+                ...state,
+                placeHistory: !state.placeHistory.includes(action.payload)
+                    ? [action.payload, ...state.placeHistory]
+                    : state.placeHistory,
+            }
+        default:
+            throw new Error('Invalid action.')
+    }
+}
+
+export { initState }
+export default reducer
