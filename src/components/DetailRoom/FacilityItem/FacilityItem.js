5fd46ab0df74ed092dc3933c883588cc372bb506
Vylee <87347391+Vylee29@users.noreply.github.com>
2023-04-30 09:56:46 +0700

    update list facility with call db

--- src/components/DetailRoom/FacilityItem/FacilityItem.js
+++ src/components/DetailRoom/FacilityItem/FacilityItem.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import classes from './FacilityItem.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '@fortawesome/fontawesome-free'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
 const FacilityItem = (props) => {
     return (
         <React.Fragment>
