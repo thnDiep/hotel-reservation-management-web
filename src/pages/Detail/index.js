94df1a1852f2d55bfad7bcca224a8ff734d3a8d8
Vylee <87347391+Vylee29@users.noreply.github.com>
2023-05-01 12:01:27 +0700

    update policy part of detail page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -3,6 +3,7 @@ import Search from '~/components/Search'
 import classes from './Detail.module.scss'
 import {
     faBath,
+    faBed,
     faBlender,
     faBoxTissue,
     faBroom,
@@ -16,10 +17,13 @@ import {
     faJugDetergent,
     faKey,
     faLuggageCart,
+    faMapMarkedAlt,
     faMugHot,
     faParking,
     faPhone,
+    faPlaneDeparture,
     faPumpSoap,
+    faRulerCombined,
     faShower,
     faSink,
     faSmokingBan,
@@ -30,16 +34,18 @@ import {
     faUserCheck,
     faUserShield,
     faUserTie,
+    faUtensilSpoon,
     faUtensils,
     faWheelchair,
     faWifi,
     faWind,
     faWineBottle,
     faWineGlass,
+    faWineGlassAlt,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ButtonPrimary from '~/components/Button/ButtonPrimary'
-import { faBell, faBuilding, faSnowflake, faSun } from '@fortawesome/free-regular-svg-icons'
+import { faBell, faBuilding, faCalendar, faSnowflake, faSun } from '@fortawesome/free-regular-svg-icons'
 import RoomsList from '~/components/DetailRoom/RoomList/RoomsList'
 import PictureDetail from './PictureDetail/PictureDetail'
 import PriceDetail from './PriceDetail/PriceDeatil'
@@ -502,31 +508,141 @@ const Detail = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className={classes.container}>
+            <hr />
+            <div className={classes.subContainer}>
                 <div className="row">
                     <div className="col-lg-12 col-md-12">
                         <div>
                             <div className={classes['box-title']}>
                                 <h3>Chính sách Khách sạn </h3>
                             </div>
-                            <div className={classes['box-policy']}>
-                                <div className={classes['panel-group']}>
-                                    <div className={classes.panel}>
-                                        <div className="card-header">
-                                            <a className="btn" data-bs-toggle="collapse" href="#collapseOne">
-                                                Collapsible Group Item #1
-                                            </a>
-                                        </div>
-                                        <div id="collapseOne" className="collapse show" data-bs-parent="#accordion">
-                                            <div class="card-body">Lorem ipsum..</div>
-                                        </div>
+                            <div className={classes.hotelPolicyHour}>
+                                <div className={classes.hour}>
+                                    <div className={classes.CheckInAndOut}>
+                                        <div>Nhận phòng</div>
+                                        <div className={classes.hourPolicy}>Từ 14:00</div>
+                                    </div>
+                                </div>
+                                <div className={classes.hour}>
+                                    <div className={classes.CheckInAndOut}>
+                                        <div>Trả phòng</div>
+                                        <div className={classes.hourPolicy}>Trước 12:00</div>
                                     </div>
                                 </div>
                             </div>
+                            <div className={classes.hotelPolicyRegular}>
+                                <div className={classes.regular}>
+                                    <span className={classes.regularTitle}>Chính sách chung</span>
+                                    <div className={classes.regularContent}>
+                                        <p>Không cho phép hút thuốc</p>
+                                        <p>Không cho phép thú cưng</p>
+                                        <p>
+                                            Hạn chế người Việt Nam và người nước ngoài check-in cùng 1 phòng, nếu không
+                                            xuất trình giấy đăng ký kết hôn
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={classes.regular}>
+                                    <span className={classes.regularTitle}>Chính sách trẻ em</span>
+                                    <div className={classes.regularContent}>
+                                        <p>Trẻ em từ 12 tuổi sẽ được xem như người lớn</p>
+                                        <p>
+                                            Quý khách hàng vui lòng nhập đúng số lượng khách và tuổi để có giá chính
+                                            xác.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr />
                         </div>
                     </div>
                 </div>
-            </div> */}
+                <div className="row">
+                    <div className="col-lg-12 col-md-12 mt-1">
+                        <div>
+                            <div className={classes['box-title']}>
+                                <h3>Thông tin hữu ích </h3>
+                            </div>
+                            <div className={classes.usefulInformation}>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faBuilding} />
+                                        </span>
+                                        <span className={classes.text}>Khoảng cách tới trung tâm</span>
+                                    </div>
+                                    <span>--:--km</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faPlaneDeparture} />
+                                        </span>
+                                        <span className={classes.text}>Khoảng cách tới sân bay</span>
+                                    </div>
+                                    <span>--:--km</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faMapMarkedAlt} />
+                                        </span>
+                                        <span className={classes.text}>Khoảng cách tới trung tâm thị trấn</span>
+                                    </div>
+                                    <span>--:--km</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faCalendar} />
+                                        </span>
+                                        <span className={classes.text}>Năm khách sạn xây dựng</span>
+                                    </div>
+                                    <span>2018</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faWineGlassAlt} />
+                                        </span>
+                                        <span className={classes.text}>Số quán bar</span>
+                                    </div>
+                                    <span>2</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faBed} />
+                                        </span>
+                                        <span className={classes.text}>Số phòng khách sạn</span>
+                                    </div>
+                                    <span>80</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faUtensils} />
+                                        </span>
+                                        <span className={classes.text}>Số nhà hàng</span>
+                                    </div>
+                                    <span>2</span>
+                                </div>
+                                <div className={classes.item}>
+                                    <div className={classes.partOfItem}>
+                                        <span>
+                                            <FontAwesomeIcon icon={faRulerCombined} />
+                                        </span>
+                                        <span className={classes.text}>Số tầng khách sạn</span>
+                                    </div>
+                                    <span>15</span>
+                                </div>
+                            </div>
+
+                            <hr />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
