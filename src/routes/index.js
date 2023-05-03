b1e3e2e42d0f34496f2ba698099364df848b904b
Tran-thaoquyen23 <Tranthaoquyen778623@gmail.com>
2023-05-03 23:52:47 +0700

    Complete OrderDetail Page UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,9 @@ import Checkout from '~/pages/Checkout'
 import Profile from '~/pages/Profile'
 import QrPay from '~/pages/QrPay'
 import AdminHotel from '~/pages/Admin/Hotel'
-import { HomeLayout, QrPayLayout, AdminLayout } from '~/components/Layouts'
 import RegisterPartner from '~/pages/RegisterPartner/RegisterPartner'
+import OrderDetail from '~/pages/Profile/OrderDetail'
+import { HomeLayout, QrPayLayout, AdminLayout } from '~/components/Layouts'
 
 export const publicRoutes = [
     { path: '/', component: Home, layout: HomeLayout },
@@ -14,6 +15,7 @@ export const publicRoutes = [
     { path: '/listByPlace', component: ListByPlace },
     { path: '/checkout', component: Checkout, layout: HomeLayout },
     { path: '/profile', component: Profile },
+    { path: '/profile/order', component: OrderDetail },
     { path: '/qrPay', component: QrPay, layout: QrPayLayout },
     { path: '/admin/hotel', component: AdminHotel, layout: AdminLayout },
     { path: '/registerPartner', component: RegisterPartner, layout: QrPayLayout },
