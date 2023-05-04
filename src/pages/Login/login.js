45a1cbadd95c093f74cdf7e50aae1091dc03ead0
Tran-thaoquyen23 <92144715+Tran-thaoquyen23@users.noreply.github.com>
2023-05-04 23:56:06 +0700

    complete login ui

---
+++ src/pages/Login/login.js
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react'
+import styles from './login.module.scss'
+
+function Login() {
+    const data = JSON.parse(localStorage.getItem('identification'))
+
+    const [email, setEmail] = useState(() => {
+        return { value: '', error: ' ', isValid: false }
+    })
+
+    useEffect(() => {
+        const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+        if (email.isValid === true) {
+            const identifier = setTimeout(() => {
+                if (!filter.test(email.value)) {
+                    setEmail({ ...email, error: 'Email không đúng ' })
+                } else if (email.value.length === 0) {
+                    setEmail({ ...email, error: 'Thông tin bắt buộc' })
+                } else {
+                    setEmail({ ...email, error: '' })
+                }
+            }, 500)
+            return () => {
+                clearTimeout(identifier)
+            }
+        }
+    }, [email.value])
+
+    function handleLogin() {}
+
+    return (
+        <div className={styles.wrap}>
+            <div className={styles.introduction}>
+                <div className={styles.infoWeb}>
+                    <img
+                        className={styles.logo}
+                        src="https://static.vecteezy.com/system/resources/previews/016/892/364/original/hotel-icon-free-vector.jpg"
+                    />
+                    <div>
+                        <p className={styles.nameOfWeb}>MyTravel</p>
+                        <p className={styles.description}>Hotels at your fingertips</p>
+                    </div>
+                </div>
+                <p className={styles.title1}>Welcome back!</p>
+                <p className={styles.title2}>Solution for a great trip</p>
+                <img
+                    className={styles.image}
+                    src="https://travl.dexignlab.com/react/demo/static/media/pic1.5182ad472b66273c2b70.png"
+                />
+            </div>
+            <div className={styles.triangleDown}></div>
+            <div className={styles.triangleUp}></div>
+            <div className={styles.login}>
+                <div>
+                    <p className={styles.title}>Đăng nhập vào tài khoản</p>
+                    <div>
+                        <label className={styles.label}>Email</label>
+                        <div>
+                            <input
+                                className={styles.fieldInput}
+                                id="field-name"
+                                name="email"
+                                type="text"
+                                placeholder="Nhập email"
+                                onChange={(e) => setEmail({ ...email, value: e.target.value, isValid: true })}
+                            />
+                        </div>
+                        <p className={styles.err}>{email.error}</p>
+                    </div>
+                    <div>
+                        <label className={styles.label}>Mật khẩu</label>
+                        <div>
+                            <input
+                                className={styles.fieldInput}
+                                id="field-name"
+                                name="password"
+                                type="password"
+                                placeholder="Nhập mật khẩu"
+                            />
+                        </div>
+                    </div>
+
+                    <form action="/">
+                        <input type="button" value="Đăng nhập" onClick={handleLogin} className={styles.loginBtn} />
+                    </form>
+
+                    <p className={styles.title3}>
+                        <p>
+                            <a className={styles.ggLink}>Đăng nhập bằng Google</a>
+                        </p>
+                        Bạn không có tài khoản?
+                        <a className={styles.signUpLink}>Đăng ký</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
+import Login from '~/pages/Login/login'
 import Home from '~/pages/Home'
 import Detail from '~/pages/Detail'
 import ListByPlace from '~/pages/ListByPlace'
 import Checkout from '~/pages/Checkout'
 import Profile from '~/pages/Profile'
+import Reserve from '~/pages/Profile/reservePage/reserve'
+import Wishlist from '~/pages/Profile/wishlistPage/wishlist'
 import QrPay from '~/pages/QrPay'
 import AdminHotel from '~/pages/Admin/Hotel'
 import { HomeLayout, QrPayLayout, AdminLayout } from '~/components/Layouts'
@@ -14,7 +17,10 @@ export const publicRoutes = [
     { path: '/listByPlace', component: ListByPlace },
     { path: '/checkout', component: Checkout, layout: HomeLayout },
     { path: '/profile', component: Profile },
+    { path: '/reserve', component: Reserve },
+    { path: '/wishlist', component: Wishlist },
     { path: '/qrPay', component: QrPay, layout: QrPayLayout },
     { path: '/admin/hotel', component: AdminHotel, layout: AdminLayout },
     { path: '/registerPartner', component: RegisterPartner, layout: QrPayLayout },
+    { path: '/login', component: Login, layout: null },
 ]
